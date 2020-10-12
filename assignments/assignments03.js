45000f65f22b7479cf47e7064b52fc26c2221e1f
Zielke2401 <70655896+Zielke2401@users.noreply.github.com>
2020-10-11 22:09:25 -0400

    Update assignments03.js

--- assignments/assignments03.js
+++ assignments/assignments03.js
@@ -13,6 +13,384 @@ elem[1].children[4].children[0].onclick = function(){
   = add1(elem[1], elem[19]);
 };
 
+elem[1].children[4].children[1].onclick = function(){
+  = add1(elem[1], elem[19]);
+};
+
+elem[1].children[4].children[2].onclick = function(){
+  = add1(elem[1], elem[19]);
+};
+
+
+elem[2]
+  = document.getElementById("2");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[2].children[4].children[0].onclick = function(){
+  = add1(elem[2], elem[19]);
+};
+
+elem[2].children[4].children[1].onclick = function(){
+  = add1(elem[2], elem[19]);
+};
+
+elem[2].children[4].children[2].onclick = function(){
+  = add1(elem[2], elem[19]);
+};
+
+elem[3]
+  = document.getElementById("3");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[3].children[4].children[0].onclick = function(){
+  = add1(elem[3], elem[19]);
+};
+
+elem[3].children[4].children[1].onclick = function(){
+  = add1(elem[3], elem[19]);
+};
+
+elem[3].children[4].children[2].onclick = function(){
+  = add1(elem[3], elem[19]);
+};
+
+elem[4]
+  = document.getElementById("4");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[4].children[4].children[0].onclick = function(){
+  = add1(elem[4], elem[19]);
+};
+
+elem[4].children[4].children[1].onclick = function(){
+  = add1(elem[4], elem[19]);
+};
+
+elem[4].children[4].children[2].onclick = function(){
+  = add1(elem[4], elem[19]);
+};
+
+
+elem[5]
+  = document.getElementById("5");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[5].children[4].children[0].onclick = function(){
+  = add1(elem[5], elem[19]);
+};
+
+elem[5].children[4].children[1].onclick = function(){
+  = add1(elem[5], elem[19]);
+};
+
+elem[5].children[4].children[2].onclick = function(){
+  = add1(elem[5], elem[19]);
+};
+
+
+elem[6]
+  = document.getElementById("6");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[6].children[4].children[0].onclick = function(){
+  = add1(elem[6], elem[19]);
+};
+
+elem[6].children[4].children[1].onclick = function(){
+  = add1(elem[6], elem[19]);
+};
+
+elem[6].children[4].children[2].onclick = function(){
+  = add1(elem[6], elem[19]);
+};
+
+
+elem[7]
+  = document.getElementById("7");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[7].children[4].children[0].onclick = function(){
+  = add1(elem[7], elem[19]);
+};
+
+elem[7].children[4].children[1].onclick = function(){
+  = add1(elem[7], elem[19]);
+};
+
+elem[7].children[4].children[2].onclick = function(){
+  = add1(elem[7], elem[19]);
+};
+
+
+elem[8]
+  = document.getElementById("8");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[8].children[4].children[0].onclick = function(){
+  = add1(elem[8], elem[19]);
+};
+
+elem[8].children[4].children[1].onclick = function(){
+  = add1(elem[8], elem[19]);
+};
+
+elem[8].children[4].children[2].onclick = function(){
+  = add1(elem[8], elem[19]);
+};
+
+
+elem[9]
+  = document.getElementById("9");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[9].children[4].children[0].onclick = function(){
+  = add1(elem[9], elem[19]);
+};
+
+elem[9].children[4].children[1].onclick = function(){
+  = add1(elem[9], elem[19]);
+};
+
+elem[9].children[4].children[2].onclick = function(){
+  = add1(elem[9], elem[19]);
+};
+
+elem[10]
+  = document.getElementById("10");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[10].children[4].children[0].onclick = function(){
+  = add1(elem[10], elem[19]);
+};
+
+elem[10].children[4].children[1].onclick = function(){
+  = add1(elem[10], elem[19]);
+};
+
+elem[10].children[4].children[2].onclick = function(){
+  = add1(elem[10], elem[19]);
+};
+
+elem[11]
+  = document.getElementById("11");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[11].children[4].children[0].onclick = function(){
+  = add1(elem[11], elem[19]);
+};
+
+elem[11].children[4].children[1].onclick = function(){
+  = add1(elem[11], elem[19]);
+};
+
+elem[11].children[4].children[2].onclick = function(){
+  = add1(elem[11], elem[19]);
+};
+
+elem[12]
+  = document.getElementById("12");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[12].children[4].children[0].onclick = function(){
+  = add1(elem[12], elem[19]);
+};
+
+elem[12].children[4].children[1].onclick = function(){
+  = add1(elem[12], elem[19]);
+};
+
+elem[12].children[4].children[2].onclick = function(){
+  = add1(elem[12], elem[19]);
+};
+
+
+elem[13]
+  = document.getElementById("13");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[13].children[4].children[0].onclick = function(){
+  = add1(elem[13], elem[19]);
+};
+
+elem[13].children[4].children[1].onclick = function(){
+  = add1(elem[13], elem[19]);
+};
+
+elem[13].children[4].children[2].onclick = function(){
+  = add1(elem[13], elem[19]);
+};
+
+elem[14]
+  = document.getElementById("14");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[14].children[4].children[0].onclick = function(){
+  = add1(elem[14], elem[19]);
+};
+
+elem[14].children[4].children[1].onclick = function(){
+  = add1(elem[14], elem[19]);
+};
+
+elem[14].children[4].children[2].onclick = function(){
+  = add1(elem[14], elem[19]);
+};
+
+
+elem[15]
+  = document.getElementById("15");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[15].children[4].children[0].onclick = function(){
+  = add1(elem[15], elem[19]);
+};
+
+elem[15].children[4].children[1].onclick = function(){
+  = add1(elem[15], elem[19]);
+};
+
+elem[15].children[4].children[2].onclick = function(){
+  = add1(elem[15], elem[19]);
+};
+
+
+elem[16]
+  = document.getElementById("16");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[16].children[4].children[0].onclick = function(){
+  = add1(elem[16], elem[19]);
+};
+
+elem[16].children[4].children[1].onclick = function(){
+  = add1(elem[16], elem[19]);
+};
+
+elem[16].children[4].children[2].onclick = function(){
+  = add1(elem[16], elem[19]);
+};
+
+
+elem[17]
+  = document.getElementById("17");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[17].children[4].children[0].onclick = function(){
+  = add1(elem[17], elem[19]);
+};
+
+elem[17].children[4].children[1].onclick = function(){
+  = add1(elem[17], elem[19]);
+};
+
+elem[17].children[4].children[2].onclick = function(){
+  = add1(elem[17], elem[19]);
+};
+
+
+elem[18]
+  = document.getElementById("18");
+
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[18].children[4].children[0].onclick = function(){
+  = add1(elem[6], elem[19]);
+};
+
+elem[18].children[4].children[1].onclick = function(){
+  = add1(elem[18], elem[19]);
+};
+
+elem[18].children[4].children[2].onclick = function(){
+  = add1(elem[18], elem[19]);
+};
+
+
+
 elem[19].children[1].innerHTML = totalPar; 
 
 // create an "add1" function
